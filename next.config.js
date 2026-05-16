55cb4b098c2595962aff37dad9959eedc5755519
cybersecuritybhau-maker <cybersecuritybhau@gmail.com>
2026-05-16 23:27:06 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fizzer.vercel.app',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
